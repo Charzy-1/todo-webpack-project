c8c1037f6f0a324bfad79329f3c32be72c26c590
Charles <chazylites@gmail.com>
2024-07-18 12:01:01 +0100

    added the complete button functionality

---
+++ src/statusUpdates.js
@@ -0,0 +1,11 @@
+// Function to toggle task completion status
+const toggleTaskStatus = (tasks, index) => {
+  tasks[index].completed = !tasks[index].completed;
+};
+
+// Function to clear all completed tasks
+const clearCompletedTasks = (tasks) => {
+  return tasks.filter(task => !task.completed);
+};
+
+export default { toggleTaskStatus, clearCompletedTasks };
